10974e1b7625eae9ce881f4044652ea238e91287
RaulOrtiz99 <raulalbertoortiz290199@hotmail.com>
2024-05-28 22:28:57 -0400

    ultimos cambios antes de la siguiente clase

---
+++ lib/pages/homepage/homepage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/welcomepage/welcomepage.dart
@@ -0,0 +1,245 @@
+import 'package:flutter/material.dart';
+// ignore_for_file: prefer_const_constructors
+
+
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(86, 18, 110, 186),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(30),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Bienvenido",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Registrarse",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  Text(
+                    "Iniciar Sesión",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.white,
+                      ),
+                      focusColor: Colors.white,
+                      fillColor: Colors.white,
+                      labelText: "Correo",
+                      labelStyle: TextStyle(
+                        color: Colors.white,
+                      )),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.white,
+                      ),
+                      focusColor: Colors.white,
+                      fillColor: Colors.white,
+                      labelText: "Contrasenia",
+                      labelStyle: TextStyle(
+                        color: Colors.white,
+                      )),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.white,
+                      ),
+                      focusColor: Colors.white,
+                      fillColor: Colors.white,
+                      labelText: "Confirmar contrasenia",
+                      labelStyle: TextStyle(
+                        color: Colors.white,
+                      )),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 370,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(86, 18, 110, 186),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Center(
+                    child: Text(
+                  'Registrarse',
+                  style: TextStyle(color: Colors.white, fontSize: 20),
+                )),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Divider(
+                    color: Colors.white,
+                    height: 10,
+                  ),
+                  Text("O registrarse con",
+                      style: TextStyle(color: Colors.white)),
+                  Divider(
+                    color: Colors.white,
+                  )
+                ],
+              ),
+          SizedBox(height: 20,),
+                  
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(86, 18, 110, 186),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/googlelogo.png',
+                            height: 50,
+                          ),
+                          Text(
+                            "Google",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(86, 18, 110, 186),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/applelogo.png',
+                            height: 50,
+                          ),
+                          Text(
+                            "Apple",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(86, 18, 110, 186),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/facebooklogo.png',
+                            height: 50,
+                          ),
+                          Text(
+                            "Facebook",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "¿Ya tienes una cuenta?",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  GestureDetector(
+                    onTap: (){},
+                    child: Text(
+                      "Iniciar Sesión",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
